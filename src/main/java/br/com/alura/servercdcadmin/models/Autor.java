820ef0093ca3bd0eca893a518dad87de6f2eaeea
Sandro Oscar Bugmann <prof.sandrob@gmail.com>
2020-02-03 21:43:39 -0300

    ajuste campo senha - deve ser atribuido no json

--- src/main/java/br/com/alura/servercdcadmin/models/Autor.java
+++ src/main/java/br/com/alura/servercdcadmin/models/Autor.java
@@ -6,19 +6,19 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 @Valid
 public class Autor {
 	
 	private Integer id;
+	
 	@NotNull(message="Nome é obrigatório") 
 	@NotEmpty(message="Nome não pode ser vazio")
 	private String nome;
+	
 	@NotEmpty(message="Nome não pode ser vazio")
 	@NotNull(message="E-mail é obrigatório") 
 	private String email;
-	@JsonIgnore
+
 	@NotEmpty(message="Senha não pode ser vazia")
 	@NotNull(message="Senha é obrigatório") 
 	@Size(min=3, message="Senha deve conter no minimo 3 digitos")
